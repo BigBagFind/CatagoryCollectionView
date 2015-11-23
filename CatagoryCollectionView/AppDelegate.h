2a80163de3ae201226ffae8e322d189bd67774a3
BigBagFind <594228924@qq.com>
2015-11-23 17:17:59 +0800

    Initial Commit

---
+++ CatagoryCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CatagoryCollectionView
+//
+//  Created by 吴玉铁 on 15/11/23.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
